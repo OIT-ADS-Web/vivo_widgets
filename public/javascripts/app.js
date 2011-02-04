e45d46df03f3751ed429bdd90c5415aab2cbebd5
Jeremy Bandini <jeremy.bandini@duke.edu>
2011-02-04 14:08:45 -0500

    refactor url generation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,14 +2,14 @@ function fetchPreview(url) {
     $('#host').hide("drop", {  }, 150, function() {
         $('#loading').show("drop", { }, 150);
 
-        $.getJSON(url + "&callback=?");
+        $.getJSON(url[0] + '.jsonp' + url[1] + "&callback=?");
 
     });
 }
 
 function vivoWidgetResult(data) {
     if(data.results.length > 0) {
-        var resultHtml = ["<ul>"];
+        var resultHtml = ["<h1>Preview Settings</h1><ul>"];
         $.each(data.results, function(i,item) {
             resultHtml.push('<li>' + item.citation + '</li>');
         });
@@ -21,62 +21,47 @@ function vivoWidgetResult(data) {
 
         });
     } else {
-      $("#host").html('Move along, there are no results to see here...');
+        $("#host").html('Move along, there are no results to see here...');
     }
 
 }
 
-
-// var format = [{name: 'abbreviated', value: 'abbr'}, {name: 'detailed', 'detail'}];
-var viewModel = {
-
-    personId: 1234,
-    chosenCollection: ko.observable(collections),
-    chosenLimit: ko.observable(noItems),
-    chosenFormat: ko.observable('abbreviated'),
-    chosenStyle: ko.observable('yes'),
-    latestUrl: ''
-
-};
 function openHelp(helpId) {
-     var $dialog = $('<div></div>')
+    var $dialog = $('<div></div>')
     .html(help[helpId].details)
     .dialog({
-      autoOpen: false,
-      title: help[helpId].title
+        autoOpen: false,
+        title: help[helpId].title
     });
-$dialog.dialog('open');
+    $dialog.dialog('open');
 }
-$(function() {
+
+$( function() {
     ko.applyBindings(viewModel);
 
     viewModel.url = ko.dependentObservable( function() {
-        latestUrl = 'http://localhost:9000/people/smithjm/publications/' + this.chosenLimit().label + '.jsonp?collections=' 
-          + this.chosenCollection().collectionName 
-          // + '&items=' + this.chosenLimit().label 
-          + '&format=' + this.chosenFormat()
-          + '&style=' + this.chosenStyle();
+        latestUrl = 'http://localhost:9000/people/smithjm/publications/' + this.chosenLimit().label;
+        latestParams = '?collections='
+        + this.chosenCollection().collectionName
+        // + '&items=' + this.chosenLimit().label
+        + '&format=' + this.chosenFormat()
+        + '&style=' + this.chosenStyle();
 
-        fetchPreview(latestUrl);
-        var script = '<script type="text/javascript" src="' + latestUrl + '"> <\/script>';
+        fetchPreview([latestUrl, latestParams]);
+        var script = '<script type="text/javascript" src="' + latestUrl + '.js' + latestParams + '"> <\/script>';
         $('#embed').val(script);
         //this.latestUrl = latestUrl;
-        return latestUrl;
+        return [latestUrl, latestParams];
     }, viewModel);
     $('#preview').click( function() {
-              fetchPreview(viewModel.url());
+        fetchPreview(viewModel.url());
         return false;
     });
-    
-    
-    
-
-  $('.help').click(function() {
-    //$dialog.dialog('open');
-    openHelp(this.id);
-    // prevent the default action, e.g., following a link
-    return false;
-  });
-
+    $('.help').click( function() {
+        //$dialog.dialog('open');
+        openHelp(this.id);
+        // prevent the default action, e.g., following a link
+        return false;
+    });
 })
 
