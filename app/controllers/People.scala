c88e235c856045ad0490f32578d0dbae1be751af
Jim Wood <jim.wood@duke.edu>
2011-02-04 10:29:34 -0500

    FDP-599: Added stub action and route for JSONP version of publication response

--- app/controllers/People.scala
+++ app/controllers/People.scala
@@ -7,6 +7,18 @@ import models._
 
 object People extends Controller {
 
+  def publicationsStub(vivoId: String) = {
+    val resultMock = """
+        vivoWidgetResult({
+          "resultHeader" : {"name" : "Smith, Joseph M", "id" : "smithjm", "date" : "2011-02-04"},
+          "results"      : [{"citation":"Smith, JM, Cook, DE, Rogers, MR. Scala for Dummies. Webposse Press, 2007."},
+                            {"citation":"Smith, JM, Cook, DE, Rogers, MR. Scala for Dummies. Webposse Press, 2007."},
+                            {"citation":"Lawrence GL, Smith J (2010). Scala and Concurrency. Scaling Times. 5(15). 34-37."}]
+        });
+    """
+    Json(resultMock)
+  }
+
   def publications(vivoId: String) = {
     Publication.findAllForPerson(Vivo.baseUri+vivoId) match {
       case Some(publications) => Template("publications" -> publications)
