c7e25cc2fd67217e19448f6f6ab6af4a5b98ac6e
Jim Wood <jim.wood@duke.edu>
2011-02-16 17:16:16 -0500

    FDP-624: setup format sensitive formatting based on single html template

--- app/controllers/People.scala
+++ app/controllers/People.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play._
 import play.mvc._
+import play.templates._
 
 import models._
 
@@ -16,7 +17,22 @@ object People extends Controller {
 
   def publications(vivoId: String, items: Int) = {
     Publication.findAllForPerson(Vivo.baseUri+vivoId,items) match {
-      case Some(publications) => Template("publications" -> publications)
+      case Some(publications) =>
+        val modelData = new java.util.HashMap[java.lang.String,java.lang.Object]
+        modelData.put("publications",publications)
+        val htmlString = TemplateLoader.load("People/publications.html").render(modelData)
+        request.format.toString match {
+          case "js" => 
+            val lines = htmlString.split('\n').toList
+            val documentWrites = lines.map { "document.write('"+_+"')" }
+            val documentWritesString = documentWrites.mkString("\n")
+            Json(documentWritesString)
+
+          case "html" => Html(htmlString)
+          case _ => NoContent
+        }
+        // TODO: try make this pure Scala, something like:
+        //val strR: String =  "People/publications.html".asTemplate("publications" -> publications).getContent()
       case _ => NoContent
     }
   }
