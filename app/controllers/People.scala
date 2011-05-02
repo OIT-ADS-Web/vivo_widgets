9e1a517924d27e59dd82e2c7fece28a7f71e5784
Jim Wood <jim.wood@duke.edu>
2011-05-02 16:24:18 -0400

    FDP-676: added basic course display

--- app/controllers/People.scala
+++ app/controllers/People.scala
@@ -94,6 +94,47 @@ object People extends Controller {
     }
   }
 
+  def coursesData(vivoId: String, items: Int) = {
+    Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
+      case Some(person) => { 
+        val vwr = new VivoWidgetJsonResult(person.courses)
+        request.format match {
+          case "json" => Json(vwr.json.toString)
+          case "jsonp" => Json(vwr.jsonp)
+          case _ => NoContent
+        }
+      }
+      case _ => NotFound
+    }
+
+  }
+
+  def courses(vivoId: String, items: Int, formatting: String = "detailed", style: String = "yes") = {
+    Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
+      case Some(person) => {
+        val courses = person.courses
+        val modelData = new java.util.HashMap[java.lang.String,java.lang.Object]
+        if (items > 0) {
+          modelData.put("courses",courses.slice(0,items))
+        } else {
+          modelData.put("courses",courses)
+        }
+        modelData.put("style",style)
+        modelData.put("formatting",formatting)
+        val htmlString = TemplateLoader.load("People/courses.html").render(modelData)
+        request.format.toString match {
+          case "js" => 
+            val lines = htmlString.split('\n').toList
+            val documentWrites = lines.map { "document.write('"+_.replaceAll("'","\\\\'")+"');" }
+            val documentWritesString = documentWrites.mkString("\n")
+            Json(documentWritesString)
+          case "html" => Html(htmlString)
+          case _ => NoContent
+        }
+      }
+      case _ => "Not Found"
+    }
+  }
 }
 
 
