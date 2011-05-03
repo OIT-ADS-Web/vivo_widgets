c02b6bd54a108150659964d26ad16295d181d994
Jim Wood <jim.wood@duke.edu>
2011-05-03 10:18:17 -0400

    FDP-676: refactored repeated code in routes and people controller

--- app/controllers/People.scala
+++ app/controllers/People.scala
@@ -9,13 +9,13 @@ import edu.duke.oit.vw.solr._
 
 object People extends Controller {
 
-  def publicationsData(vivoId: String, items: Int) = {
+  def collectionData(vivoId: String, collectionName:String, items: Int) = {
     Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
       case Some(person) => { 
-        val vwr = new VivoWidgetJsonResult(person.publications)
-        request.format match {
-          case "json" => Json(vwr.json.toString)
-          case "jsonp" => Json(vwr.jsonp)
+        collectionName match {
+          case "publications" => renderCollectionData(person.publications)
+          case "grants" => renderCollectionData(person.grants)
+          case "courses" => renderCollectionData(person.courses)
           case _ => NoContent
         }
       }
@@ -24,26 +24,23 @@ object People extends Controller {
 
   }
 
-  def publications(vivoId: String, items: Int, formatting: String = "detailed", style: String = "yes") = {
+  def renderCollectionData(collection: List[AnyRef]) = {
+    val vwr = new VivoWidgetJsonResult(collection)
+    request.format match {
+      case "json" => Json(vwr.json.toString)
+      case "jsonp" => Json(vwr.jsonp)
+      case _ => NoContent
+    }
+
+  }
+
+  def collection(vivoId: String, collectionName:String, items: Int, formatting: String = "detailed", style: String = "yes") = {
     Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
       case Some(person) => {
-        val publications = person.publications.sortBy{p => p.getOrElse("year","")}.reverse
-        val modelData = new java.util.HashMap[java.lang.String,java.lang.Object]
-        if (items > 0) {
-          modelData.put("publications",publications.slice(0,items))
-        } else {
-          modelData.put("publications",publications)
-        }
-        modelData.put("style",style)
-        modelData.put("formatting",formatting)
-        val htmlString = TemplateLoader.load("People/publications.html").render(modelData)
-        request.format.toString match {
-          case "js" => 
-            val lines = htmlString.split('\n').toList
-            val documentWrites = lines.map { "document.write('"+_.replaceAll("'","\\\\'")+"');" }
-            val documentWritesString = documentWrites.mkString("\n")
-            Json(documentWritesString)
-          case "html" => Html(htmlString)
+        collectionName match {
+          case "publications" => renderCollection(collectionName,person.publications.sortBy{p => p.getOrElse("year","")}.reverse,items,formatting,style)
+          case "grants" => renderCollection(collectionName,person.grants,items,formatting,style)
+          case "courses" => renderCollection(collectionName,person.courses,items,formatting,style)
           case _ => NoContent
         }
       }
@@ -52,89 +49,27 @@ object People extends Controller {
 
   }
 
-  def grantsData(vivoId: String, items: Int) = {
-    Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
-      case Some(person) => { 
-        val vwr = new VivoWidgetJsonResult(person.grants)
-        request.format match {
-          case "json" => Json(vwr.json.toString)
-          case "jsonp" => Json(vwr.jsonp)
-          case _ => NoContent
-        }
-      }
-      case _ => NotFound
+  def renderCollection(collectionName: String, collection: List[AnyRef], items: Int, formatting: String, style: String) = {
+    val modelData = new java.util.HashMap[java.lang.String,java.lang.Object]
+    if (items > 0) {
+      modelData.put(collectionName,collection.slice(0,items))
+    } else {
+      modelData.put(collectionName,collection)
     }
-
-  }
-
-  def grants(vivoId: String, items: Int, formatting: String = "detailed", style: String = "yes") = {
-    Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
-      case Some(person) => {
-        val grants = person.grants
-        val modelData = new java.util.HashMap[java.lang.String,java.lang.Object]
-        if (items > 0) {
-          modelData.put("grants",grants.slice(0,items))
-        } else {
-          modelData.put("grants",grants)
-        }
-        modelData.put("style",style)
-        modelData.put("formatting",formatting)
-        val htmlString = TemplateLoader.load("People/grants.html").render(modelData)
-        request.format.toString match {
-          case "js" => 
-            val lines = htmlString.split('\n').toList
-            val documentWrites = lines.map { "document.write('"+_.replaceAll("'","\\\\'")+"');" }
-            val documentWritesString = documentWrites.mkString("\n")
-            Json(documentWritesString)
-          case "html" => Html(htmlString)
-          case _ => NoContent
-        }
-      }
-      case _ => "Not Found"
+    modelData.put("style",style)
+    modelData.put("formatting",formatting)
+    val htmlString = TemplateLoader.load("People/"+collectionName+".html").render(modelData)
+    request.format.toString match {
+      case "js" => 
+        val lines = htmlString.split('\n').toList
+        val documentWrites = lines.map { "document.write('"+_.replaceAll("'","\\\\'")+"');" }
+        val documentWritesString = documentWrites.mkString("\n")
+        Json(documentWritesString)
+      case "html" => Html(htmlString)
+      case _ => NoContent
     }
   }
 
-  def coursesData(vivoId: String, items: Int) = {
-    Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
-      case Some(person) => { 
-        val vwr = new VivoWidgetJsonResult(person.courses)
-        request.format match {
-          case "json" => Json(vwr.json.toString)
-          case "jsonp" => Json(vwr.jsonp)
-          case _ => NoContent
-        }
-      }
-      case _ => NotFound
-    }
-
-  }
-
-  def courses(vivoId: String, items: Int, formatting: String = "detailed", style: String = "yes") = {
-    Person.find(VivoConnection.baseUri+vivoId, SolrConnection.widgetServer) match {
-      case Some(person) => {
-        val courses = person.courses
-        val modelData = new java.util.HashMap[java.lang.String,java.lang.Object]
-        if (items > 0) {
-          modelData.put("courses",courses.slice(0,items))
-        } else {
-          modelData.put("courses",courses)
-        }
-        modelData.put("style",style)
-        modelData.put("formatting",formatting)
-        val htmlString = TemplateLoader.load("People/courses.html").render(modelData)
-        request.format.toString match {
-          case "js" => 
-            val lines = htmlString.split('\n').toList
-            val documentWrites = lines.map { "document.write('"+_.replaceAll("'","\\\\'")+"');" }
-            val documentWritesString = documentWrites.mkString("\n")
-            Json(documentWritesString)
-          case "html" => Html(htmlString)
-          case _ => NoContent
-        }
-      }
-      case _ => "Not Found"
-    }
-  }
 }
 
 
