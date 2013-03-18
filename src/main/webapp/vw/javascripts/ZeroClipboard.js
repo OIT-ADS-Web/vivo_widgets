b38dffaaaffdf206a813676f34c329a2ac6d86ad
Richard Outten (mbp) <outtenr@duke.edu>
2013-03-18 15:56:37 -0400

    FDP-1345 Fix copy to clipboard
    Fix the copy to clipboard swf that wasn't getting loaded when the
    context was under /widgets.

--- src/main/webapp/vw/javascripts/ZeroClipboard.js
+++ src/main/webapp/vw/javascripts/ZeroClipboard.js
@@ -5,7 +5,7 @@ var ZeroClipboard = {
 	
 	version: "1.0.7",
 	clients: {}, // registered upload clients on page, indexed by id
-	moviePath: '../vw/images/ZeroClipboard.swf', // URL to movie
+	moviePath: 'vw/images/ZeroClipboard.swf', // URL to movie
 	nextId: 1, // ID of next movie
 	
 	$: function(thingy) {
