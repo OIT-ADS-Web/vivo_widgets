693e9e280bc05c2865e6b593e7ed5ce24196c44d
Patrick McElwee <pmm21@duke.edu>
2014-05-15 17:02:51 -0400

    FDP-2017: WIP: Trying to set up test data in Solr

--- src/test/scala/acceptance/ArtisticWorksAPISpec.scala
+++ src/test/scala/acceptance/ArtisticWorksAPISpec.scala
@@ -1,15 +1,22 @@
 package edu.duke.oit.test.acceptance
 
 import edu.duke.oit.vw._
+import edu.duke.oit.vw.solr._
 
 import org.scalatra.test.specs2._
+import edu.duke.oit.test.helpers.{TestServers,SampleLoader}
 
 class ArtisticWorksAPISpec extends ScalatraSpec { def is =
   "The Artistic Works json feed should" ^
     "be available"                      ! be_available^
     "return nothing for bad uri"        ! return_nothing_for_bad_uri^
-    "return empty for non-artist"       ! return_empty^
+    "return empty for non-artist"       ! withSolrIndex().return_empty^
                                         end
+
+  val vivo = TestServers.vivo
+  val widgetSolr = TestServers.widgetSolr
+  val vsi = new VivoSolrIndexer(vivo, widgetSolr)
+  TestServers.loadSampleData
 
   addServlet(classOf[WidgetsServlet], "/*")
   addFilter(classOf[edu.duke.oit.vw.scalatra.WidgetsFilter], "/*")
@@ -23,8 +30,12 @@ class ArtisticWorksAPISpec extends ScalatraSpec { def is =
       body must_== "Not Found"
     }
 
-  def return_empty = 
-    get("/api/v0.9/people/artistic_works/all.json?uri=http://vivo.duke.edu/person1") {
-      body must_== "[]"
-    }
+  case class withSolrIndex {
+    vsi.indexPeople()
+
+    def return_empty = 
+      get("/api/v0.9/people/artistic_works/all.json?uri=http://vivo.duke.edu/person1") {
+        body must_== "[]"
+      }
+  }
 }
