b229e010c9c65beb8650419b1c06ab1118117994
Patrick McElwee <pmm21@duke.edu>
2014-05-16 14:18:18 -0400

    FDP-2017: Revert Scalatra version
    It was throwing an error and there was no longer any reason to upgrade

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ object VivowidgetsBuild extends Build {
   val Name = "VivoWidgets"
   val Version = "0.3.0"
   val ScalaVersion = "2.10.1"
-  val ScalatraVersion = "2.2.2"
+  val ScalatraVersion = "2.2.0"
 
   lazy val project = Project (
     "vivowidgets",
