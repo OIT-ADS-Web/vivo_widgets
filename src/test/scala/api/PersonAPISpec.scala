6e3ed0fd3b732e4ab8dfc055d8c4c60509dc69dd
Patrick McElwee <pmm21@duke.edu>
2014-11-13 17:09:56 -0500

    1.7: Flesh out Person API spec
    Only one assertion per request!?
    
    Note that Solr server currently has to be restarted between each change
    of the RDF that is being indexed. Why?

--- src/test/scala/api/PersonAPISpec.scala
+++ src/test/scala/api/PersonAPISpec.scala
@@ -12,6 +12,14 @@ import edu.duke.oit.test.helpers.TestServers
 class PersonApiSpec extends ScalatraSpec { def is = s2"""
   The Person API should
     return 'uri' $e1
+    return 'vivoType' $e2
+    return 'label' $e3
+    return 'title' $e4
+    return 'attributes(lastName)' $e5
+    return 'attributes(firstName)' $e6
+    return 'attributes(preferredTitle)' $e7
+    return 'attributes(alternateId)' $e8
+    return 'attributes(middleName)' $e9
   """
 
   addFilter(new WidgetsFilter, "/*")
@@ -24,8 +32,67 @@ class PersonApiSpec extends ScalatraSpec { def is = s2"""
   indexer.indexPeople()
 
   def e1 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    status must_== 200
     val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    println("json: " + json)
     json("uri") must_== "http://localhost/individual/n503"
   }
+
+  def e2 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    json("vivoType") must_== "http://vivoweb.org/ontology/core#FacultyMember"
+  }
+    
+  def e3 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    json("label") must_== "Smith, Richard"
+  }
+    
+  def e4 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    json("title") must_== "Programming CIO"
+  }
+    
+  def e5 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
+    println("attributes:" + attributes)
+    attributes("lastName") must_== "Smith"
+  }
+    
+  def e6 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
+    attributes("firstName") must_== "Richard"
+  }
+    
+  def e7 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
+    attributes("preferredTitle") must beEqualTo("Programming CIO")
+  }
+    
+  def e8 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
+    attributes("alternateId") must_== "0123456"
+  }
+    
+  def e9 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
+    attributes("middleName") must_== "Big"
+  }
+    
+    //attributes("primaryEmail") must_== "rsmith@example.com"
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
+    //attributes("") must_==
 }
