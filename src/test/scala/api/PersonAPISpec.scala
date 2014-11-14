976cccc39567cf99050035f4e5fb475f0b20e093
Patrick McElwee <pmm21@duke.edu>
2014-11-14 11:16:19 -0500

    1.7: Extrct spec setup

--- src/test/scala/api/PersonAPISpec.scala
+++ src/test/scala/api/PersonAPISpec.scala
@@ -8,19 +8,31 @@ import edu.duke.oit.vw.scalatra._
 import edu.duke.oit.vw.solr._
 import edu.duke.oit.vw.models._
 import edu.duke.oit.test.helpers.TestServers
+import org.specs2.specification.Step
 
 class PersonApiSpec extends ScalatraSpec { def is = s2"""
-  The Person API should
-    return 'uri' $e1
-    return 'vivoType' $e2
-    return 'label' $e3
-    return 'title' $e4
-    return 'attributes(lastName)' $e5
-    return 'attributes(firstName)' $e6
-    return 'attributes(preferredTitle)' $e7
-    return 'attributes(alternateId)' $e8
-    return 'attributes(middleName)' $e9
+  The Person API should return   ${ Step(getJson)}
+    uri                        $e1
+    vivoType                   $e2
+    label                      $e3
+    title                      $e4
+    attributes(lastName)       $e5
+    attributes(firstName)      $e6
+    attributes(preferredTitle) $e7
+    attributes(alternateId)    $e8
+    attributes(middleName)     $e9
+    attributes(primaryEmail)   $e10
   """
+
+  var json:Map[String, Any] = _
+  var attributes:Map[String, Any] = _
+
+  def getJson = {
+    get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
+      json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
+      attributes = json("attributes").asInstanceOf[Map[String, Any]]
+    }
+  }
 
   addFilter(new WidgetsFilter, "/*")
   val vivo = TestServers.vivo
@@ -31,68 +43,15 @@ class PersonApiSpec extends ScalatraSpec { def is = s2"""
 
   indexer.indexPeople()
 
-  def e1 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    println("json: " + json)
-    json("uri") must_== "http://localhost/individual/n503"
+  def e1 = { json("uri") must_== "http://localhost/individual/n503" }
+  def e2 = { json("vivoType") must_== "http://vivoweb.org/ontology/core#FacultyMember" } 
+  def e3 = { json("label") must_== "Smith, Richard" } 
+  def e4 = { json("title") must_== "Programming CIO" }
+  def e5 = { attributes("lastName") must_== "Smith" }
+  def e6 = { attributes("firstName") must_== "Richard" }
+  def e7 = { attributes("preferredTitle") must_== ("Programming CIO") }
+  def e8 = { attributes("alternateId") must_== "0123456" }
+  def e9 = { attributes("middleName") must_== "Big" }
+  def e10 = { attributes("primaryEmail") must_== "rsmith@example.com" }
+  //def e11 = { attributes("overview") must_== "This is an overview." }
   }
-
-  def e2 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    json("vivoType") must_== "http://vivoweb.org/ontology/core#FacultyMember"
-  }
-    
-  def e3 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    json("label") must_== "Smith, Richard"
-  }
-    
-  def e4 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    json("title") must_== "Programming CIO"
-  }
-    
-  def e5 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
-    println("attributes:" + attributes)
-    attributes("lastName") must_== "Smith"
-  }
-    
-  def e6 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
-    attributes("firstName") must_== "Richard"
-  }
-    
-  def e7 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
-    attributes("preferredTitle") must beEqualTo("Programming CIO")
-  }
-    
-  def e8 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
-    attributes("alternateId") must_== "0123456"
-  }
-    
-  def e9 = get("/api/v0.9/people/complete/all.json?uri=http://localhost/individual/n503") {
-    val json = JsonParser.parse(body).values.asInstanceOf[Map[String, Any]]
-    val attributes = json("attributes").asInstanceOf[Map[String, Any]]
-    attributes("middleName") must_== "Big"
-  }
-    
-    //attributes("primaryEmail") must_== "rsmith@example.com"
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-    //attributes("") must_==
-}
