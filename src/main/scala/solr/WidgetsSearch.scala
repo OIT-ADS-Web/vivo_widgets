ece4ff8cf09cf908cd3d51d131f53309b5f0f8e9
Richard Outten (mbp) <outtenr@duke.edu>
2017-04-10 15:34:37 -0400

    FDP-2913 Add alternate id
    For the modified endpoint, add the alternate id to the results that come back.

--- src/main/scala/solr/WidgetsSearch.scala
+++ src/main/scala/solr/WidgetsSearch.scala
@@ -21,7 +21,7 @@ import java.text.SimpleDateFormat
 object WidgetsSearcher extends SolrModel {
 
   def searchByUpdatedAt(since: Date, offset: Integer, solr:SolrServer): WidgetsSearchResult = {
-    // NOTE: syntax for searh in SOLR looks this: 
+    // NOTE: syntax for searh in SOLR looks this:
     // updatedAt:[2016-02-10T00:00:00Z TO NOW]
     val format = new SimpleDateFormat("YYYY-MM-dd'T'00:00:00'Z'")
     //see: http://stackoverflow.com/questions/26037324/solrj-date-request
@@ -32,7 +32,7 @@ object WidgetsSearcher extends SolrModel {
     val queryString = String.format("updatedAt:[%s TO %s] AND group:people", dateSince, dateEnd)
 
     val query = new SolrQuery()
-   
+
     query.setQuery(queryString)
     query.setShowDebugInfo(true)
 
@@ -46,7 +46,7 @@ object WidgetsSearcher extends SolrModel {
     val docList = response.getResults()
 
     val items = parseWidgetsItemList(docList)
- 
+
     var searchResult = new WidgetsSearchResult(docList.getNumFound(), docList.getStart(), items)
 
     return searchResult
@@ -56,7 +56,7 @@ object WidgetsSearcher extends SolrModel {
 
   def parseWidgetsItemList(docList: SolrDocumentList): List[WidgetsSearchResultItem] = {
     docList.toList filter  (_.get("group") != null ) map { doc =>
-      new WidgetsSearchResultItem(doc.get("id").toString)
+      new WidgetsSearchResultItem(doc.get("id").toString, doc.get("alternateId").toString)
     }
   }
 
@@ -64,4 +64,4 @@ object WidgetsSearcher extends SolrModel {
 
 class WidgetsSearchResult(val numFound: Long, val offset: Long, val  items: List[WidgetsSearchResultItem]) extends AddToJson
 
-class WidgetsSearchResultItem(val uri: String)
+class WidgetsSearchResultItem(val uri: String, val alternateId: String)
