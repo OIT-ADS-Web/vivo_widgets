6144f1b65989f48749db7526eacb1bea72103c98
ht29@duke.edu <ht29@duke.edu>
2017-12-13 12:34:19 -0500

    added dynamicfield def in solr schema

--- src/main/scala/solr/PersonIndexer.scala
+++ src/main/scala/solr/PersonIndexer.scala
@@ -93,7 +93,7 @@ object PersonIndexer extends SimpleConversion
 
       solrDoc.addField("json",personJson)
       solrDoc.addField("updatedAt", person.updatedAt.get)
-      solrDoc.addField("active_i",1)
+      solrDoc.addField("active_b",true)
       person.uris.map {uri => solrDoc.addField("uris",uri)}
      
       return Option(solrDoc)
